1f7fba69e927678f083cf403a581c3ea6165de40
Giovana-Manuquian <gmanuquian@gmail.com>
2024-05-08 22:21:24 -0300

    comeco de menu

---
+++ src/main/java/br/com/alura/screenmatch/principal/Principal.java
@@ -0,0 +1,12 @@
+package br.com.alura.screenmatch.principal;
+
+import java.util.Scanner;
+
+public class Principal {
+
+    private Scanner leitura = new Scanner(System.in);
+    public void exibeMenu(){
+        System.out.println("Digite o nome da série para a busca: ");
+        var nomeSerie = leitura.nextLine();
+    }
+}
